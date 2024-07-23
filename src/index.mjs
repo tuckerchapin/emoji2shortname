694767534b5d09687895a8863b9e323495a8791c
Tucker Chapin <tuckerchapin@gmail.com>
2024-07-23 15:10:59 -0400

    Added in variation and short name linking and short name lookups

--- src/index.mjs
+++ src/index.mjs
@@ -10,19 +10,23 @@ const emojis = await res.json();
 const fromCodePoint = s => String.fromCodePoint(...s.split('-').map(cp => parseInt(cp, 16)))
 
 const allEmojis = emojis.reduce((acc, emoji) => {
-  acc.push({
-    shortname: `:${emoji.short_name}:`,
+  const base = {
     emoji: fromCodePoint(emoji.unified),
-  });
+    shortname: `:${emoji.short_name}:`,
+    shortnames: emoji.short_names.filter(s => s !== emoji.short_name).map(s => `:${s}:`),
+  };
 
-  if (emoji.skin_variations) {
-    Object.entries(emoji.skin_variations).forEach(([tones, skin]) => {
-      acc.push({
-        shortname: `:${emoji.short_name}::skin-tone-${tones.split('-').map(c => parseInt(c.slice(-1), 16) - 9).join('-')}:`,
-        emoji: fromCodePoint(skin.unified),
-      });
-    });
-  }
+  const variations = Object.entries(emoji.skin_variations || []).map(([tones, skin]) => ({
+    emoji: fromCodePoint(skin.unified),
+    shortname: `:${emoji.short_name}::skin-tone-${tones.split('-').map(c => parseInt(c.slice(-1), 16) - 9).join('-')}:`,
+    shortnames: emoji.short_names.filter(s => s !== emoji.short_name).map(s => `:${s}::skin-tone-${tones.split('-').map(c => parseInt(c.slice(-1), 16) - 9).join('-')}:`),
+  }));
+
+  acc.push(...variations.map(v => ({
+    ...v,
+    variations: [base, ...variations.filter(e => e.shortname !== v.shortname)],
+  })));
+  acc.push({ ...base, variations });
 
   return acc;
 }, []);
@@ -53,6 +57,13 @@ const fileWrites = allEmojis.reduce((acc, emoji) => {
     fs.mkdir(shortnamePath, { recursive: true })
     .then(() => fs.writeFile(path.join(shortnamePath, 'index.json'), JSON.stringify(emoji)))
   );
+
+  // make alternative shortnames -> emoji endpoints as well
+  acc.push(...emoji.shortnames.map(shortname => {
+    const altShortnamePath = path.join(EMOJI_DIR_PATH, shortname)
+    return fs.mkdir(altShortnamePath, { recursive: true })
+    .then(() => fs.writeFile(path.join(altShortnamePath, 'index.json'), JSON.stringify(emoji)))
+  }))
 
   return acc;
 }, []);
