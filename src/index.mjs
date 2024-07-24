4334ef669aa3d49ffbfee3306facc58553a58c46
Tucker Chapin <tuckerchapin@gmail.com>
2024-07-23 22:34:47 -0400

    Ditching the folder heirarchy and .json naming...

--- src/index.mjs
+++ src/index.mjs
@@ -33,31 +33,20 @@ const allEmojis = emojis.reduce((acc, emoji) => {
 
 const OUTPUT_DIR = './_site';
 const PUBLIC_DIR = './public';
-
+await fs.rm(OUTPUT_DIR, { recursive: true, force: true });
 await fs.cp(PUBLIC_DIR, OUTPUT_DIR, { recursive: true });
+
 await fs.writeFile(path.join(OUTPUT_DIR, 'emojis.json'), JSON.stringify(allEmojis));
 
 await Promise.all(allEmojis.reduce((acc, emoji) => {
   // make emoji -> shortname endpoints
-  const emojiPath = path.join(OUTPUT_DIR, emoji.emoji)
-  acc.push(
-    fs.mkdir(emojiPath, { recursive: true })
-    .then(() => fs.writeFile(path.join(emojiPath, 'index.json'), JSON.stringify(emoji)))
-  );
+  acc.push(fs.writeFile(path.join(OUTPUT_DIR, emoji.emoji), JSON.stringify(emoji)));
 
   // make shortname -> emoji endpoints
-  const shortnamePath = path.join(OUTPUT_DIR, emoji.shortname)
-  acc.push(
-    fs.mkdir(shortnamePath, { recursive: true })
-    .then(() => fs.writeFile(path.join(shortnamePath, 'index.json'), JSON.stringify(emoji)))
-  );
+  acc.push(fs.writeFile(path.join(OUTPUT_DIR, emoji.shortname), JSON.stringify(emoji)));
 
   // make alternative shortnames -> emoji endpoints as well
-  acc.push(...emoji.shortnames.map(shortname => {
-    const altShortnamePath = path.join(OUTPUT_DIR, shortname)
-    return fs.mkdir(altShortnamePath, { recursive: true })
-    .then(() => fs.writeFile(path.join(altShortnamePath, 'index.json'), JSON.stringify(emoji)))
-  }))
+  acc.push(...emoji.shortnames.map(shortname => fs.writeFile(path.join(OUTPUT_DIR, shortname), JSON.stringify(emoji))))
 
   return acc;
 }, []));
